b14fcca2c7cafa5191060cd4328c5ff4dde947e9
LRudL <rudolf.laine@gmail.com>
2022-03-31 19:02:44 +0300

    Add GNN_full as a good general configurable GNN

--- gninvert/gnns.py
+++ gninvert/gnns.py
@@ -80,3 +80,66 @@ class GNN_3Layer(ptgeo.nn.MessagePassing):
     
     def forward(self, gdata):
         return self.propagate(gdata.edge_index, x=gdata.x)
+
+
+    
+class GNN_full(ptgeo.nn.MessagePassing):
+    def __init__(
+            self,
+            node_features, message_features,
+            message_hidden_sizes = [64],
+            message_nonlinearity = t.nn.GELU,
+            message_end_with_nonlinearity = True,
+            update_hidden_sizes = None,
+            update_nonlinearity = None,
+            update_end_with_nonlinearity = None
+    ):
+        super().__init__(aggr='add')
+        if update_hidden_sizes == None and update_nonlinearity == None and update_end_with_nonlinearity == None:
+            self.message_and_update_equal = True
+        else:
+            self.message_and_update_equal = False
+        if update_hidden_sizes == None:
+            update_hidden_sizes = message_hidden_sizes
+        if update_nonlinearity == None:
+            update_nonlinearity = message_nonlinearity
+        if update_end_with_nonlinearity == None:
+            update_end_with_nonlinearity = message_end_with_nonlinearity
+        self.m = GeneralLinearFullNet(
+            in_features = node_features * 2,
+            out_features = message_features,
+            hidden_sizes = message_hidden_sizes,
+            nonlinearity = message_nonlinearity,
+            end_with_nonlinearity = message_end_with_nonlinearity
+        )
+        self.u = GeneralLinearFullNet(
+            in_features = node_features + message_features,
+            out_features = node_features,
+            hidden_sizes = update_hidden_sizes,
+            nonlinearity = update_nonlinearity,
+            end_with_nonlinearity = update_end_with_nonlinearity
+        )
+        self.node_features = node_features
+        self.message_features = message_features
+        self.message_hidden_size = message_hidden_sizes
+        self.update_hidden_size = update_hidden_sizes
+        self.message_nonlinearity = message_nonlinearity
+        self.update_nonlinearity = update_nonlinearity
+        self.message_end_with_nonlinearity = message_end_with_nonlinearity
+        self.update_end_with_nonlinearity = update_end_with_nonlinearity
+    
+    def message(self, x_i, x_j):
+        inputs = t.cat([x_i, x_j], 1)
+        # ^ [[x_i[0], x_j[0]], ...]
+        return self.m(inputs)
+    
+    def update(self, aggr_out, x=None):
+        assert x is not None
+        # aggr_out is the result of applying
+        # the aggregation function (specified in the aggr
+        # argument to super()) to all of the incoming messages
+        inputs = t.cat([x, aggr_out], 1)
+        return self.u(inputs)
+    
+    def forward(self, gdata):
+        return self.propagate(gdata.edge_index, x=gdata.x)
